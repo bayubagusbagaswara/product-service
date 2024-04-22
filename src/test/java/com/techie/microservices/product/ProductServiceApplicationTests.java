f30f1eacb3be85907e9eac7c75bc534fcf35dc77
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-23 06:05:17 +0700

    update unit test

--- src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
+++ src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
@@ -2,9 +2,12 @@ package com.techie.microservices.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
+
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongodb-example");
 
 	@Test
 	void contextLoads() {
